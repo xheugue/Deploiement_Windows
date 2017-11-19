f7f0b3bef14b9267f4700bec0e7463650557eb42
Xavier Heugue <xavier.heugue@gmail.com>
2017-11-19 18:48:23 +0100

    Trouve toutes les clés liés à un programme

--- registry/SoftwareInformationsProvider.pm
+++ registry/SoftwareInformationsProvider.pm
@@ -56,7 +56,7 @@ sub new {
 
     bless($this, $class);
 
-    $this->{keys} = new Win32::TieRegistry("HKEY_LOCAL_MACHINE\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall", { Access=>KEY_READ() });
+    $this->{keys} = new Win32::TieRegistry("HKEY_LOCAL_MACHINE\\Software", { Access=>KEY_READ() });
     return $this;
 }
 
@@ -108,16 +108,107 @@ sub _listSoftwareInSubkeys {
     return @program;
 }
 
-sub getSoftwareList() {
+sub getSoftwareList {
     my @args = @_;
 
     die ("Not enough parameters to get the software list") if (@args != 1);
 
     my ($this) = @args;
 
-    my @programs = $this->_listSoftwareInSubkeys();
+    my $subkey = _createFromKey SoftwareInformationsProvider($this->{keys}, "Microsoft\\Windows\\CurrentVersion\\Uninstall");
+    my @programs = $subkey->_listSoftwareInSubkeys();
     my $program = \@programs;
     return $program;
+}
+
+sub _getKeyPathByDisplayName {
+    my @args = @_;
+
+    die ("Not enough parameters to get the software list") if (@args != 2);
+
+    my ($this, $displayname) = @args;
+
+    my @subkeyNames = $this->{keys}->SubKeyNames();
+    my @program;
+
+    foreach my $keyName (@subkeyNames) {
+        my $subkey = _createFromKey SoftwareInformationsProvider($this->{keys}, $keyName);
+
+        if (! defined($subkey->{keys})) {
+            next;
+        }
+
+        my @valueNames = $subkey->{keys}->ValueNames();
+
+        my @filteredAttribute = grep(m/DisplayName/, @valueNames);
+
+        if (@filteredAttribute == 1) {
+            my ($value) = $subkey->{keys}->GetValue("DisplayName");
+
+            if ($value eq $displayname) {
+                return $subkey->{keys}->Path;
+            }
+        } else {
+            next;
+        }
+    }
+    return undef;
+}
+
+sub _searchKeysWithKeyNameAndKeyValues {
+    my @args = @_;
+
+    die ("Not enough parameters to get the software list") if (@args != 3);
+
+    my ($this, $keyName, $displayname) = @args;
+
+    my @subkeys = $this->{keys}->SubKeyNames();
+
+    my @keys;
+
+    foreach my $subkeyName (@subkeys) {
+        if ($subkeyName =~ m/($displayname|$keyName)/gi) {
+            push(@keys, $this->{keys}->Path . $subkeyName);
+        }
+
+        my $subkey = _createFromKey SoftwareInformationsProvider($this->{keys}, $subkeyName);
+
+        if (! defined($subkey->{keys})) {
+            next;
+        }
+
+        my @valueNames = $subkey->{keys}->ValueNames();
+        my $isIn = undef;
+
+        foreach my $valueName (@valueNames) {
+            if (( ($valueName =~ m/($displayname|$keyName)/gi) || ($subkey->{keys}->GetValue($valueName) =~ m/($displayname|$keyName)/gi) ) && ! defined($isIn)) {
+                push(@keys, $subkey->{keys}->Path);
+                $isIn = 1;
+            }
+        }
+        if ($subkey->{keys}->SubKeyNames() != 0) {
+            push(@keys, $subkey->_searchKeysWithKeyNameAndKeyValues($keyName, $displayname));
+        }
+    }
+    return @keys;
+}
+
+sub getSoftwareRelatedKeys {
+    my @args = @_;
+
+    die ("Not enough parameters to get the software list") if (@args != 2);
+
+    my ($this, $displayname) = @args;
+
+    my $subkey = _createFromKey SoftwareInformationsProvider($this->{keys}, "Microsoft\\Windows\\CurrentVersion\\Uninstall");
+
+    my $keyPath = $subkey->_getKeyPathByDisplayName($displayname);
+
+    my ($keyName) = $keyPath =~ m/[^\\]+?\\$/g;
+
+    my $backslash = chop($keyName);
+
+    return $this->_searchKeysWithKeyNameAndKeyValues($keyName, $displayname);
 }
 
 1;
